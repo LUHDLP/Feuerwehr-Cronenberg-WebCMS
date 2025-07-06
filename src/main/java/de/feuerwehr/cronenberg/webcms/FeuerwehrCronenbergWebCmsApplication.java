8b9bf814a9f9e485391a13d785609109cc1ab62e
Luca <kabilkaluca118@gmail.com>
2025-07-06 18:25:48 +0200

    Initial commit

---
+++ src/main/java/de/feuerwehr/cronenberg/webcms/FeuerwehrCronenbergWebCmsApplication.java
@@ -0,0 +1,13 @@
+package de.feuerwehr.cronenberg.webcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeuerwehrCronenbergWebCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeuerwehrCronenbergWebCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/feuerwehr/cronenberg/webcms/FeuerwehrCronenbergWebCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.feuerwehr.cronenberg.webcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeuerwehrCronenbergWebCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
